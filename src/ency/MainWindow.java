fb29b487f2d661866044131a454813e84a8c001a
EWLameijer <eric_wubbo@hotmail.com>
2016-07-15 19:19:38 +0200

    Second commit - creating front window

--- src/ency/MainWindow.java
+++ src/ency/MainWindow.java
@@ -3,62 +3,79 @@ package ency;
 import java.awt.GridBagConstraints;
 import java.awt.GridBagLayout;
 
+import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JTextArea;
 import javax.swing.JTextField;
 
 /**
- * [CC] Creates the main window in which to view and edit the encyclopedia.
+ * Creates the main window in which to view and edit the encyclopedia.
  * 
  * @author Eric-Wubbo Lameijer
  */
 public class MainWindow extends JFrame {
 
-	// [CCCC] the serial version UID - not used but kind of standard for
-	// Java classes
-	private static final long serialVersionUID = 428891413938848456L;
+  // the serial version UID - not used but kind of standard for
+  // Java classes
+  private static final long serialVersionUID               = 428891413938848456L;
 
-	// @@@[the text field in which you type the entry you want to create or edit
-	// (like "GridBagLayout")
-	private JTextField m_findEntryField;
+  // @@@[the text field in which you type the entry you want to create or edit
+  // (like "Dopamine")
+  private JTextField        m_findEntryField               = new JTextField();
+  private JLabel            m_previousEntry                = new JLabel();
+  private JLabel            m_currentEntry                 = new JLabel();
+  private JLabel            m_nextEntry                    = new JLabel();
+  private JTextArea         m_currentText                  = new JTextArea();
+  private JTextField        m_findTermField                = new JTextField();
+  private JButton           m_findPreviousOccurrenceButton = new JButton("<-");
+  private JButton           m_findNextOccurrenceButton     = new JButton("->");
 
-	/**
-	 * @@@[
-	 * 
-	 *      @return
-	 */
-	private GridBagConstraints createSimpleGridBagConstraints(int columnIndex,
-	    int rowIndex, int widthInColumns, int heightInRows) {
-		GridBagConstraints gridBagConstraints = new GridBagConstraints();
-		gridBagConstraints.gridx = columnIndex;
-		gridBagConstraints.gridy = rowIndex;
+  /**
+   * Creates the gridbag-constraints for a widget.
+   * 
+   * @return GridBagConstraints that are based on the input values as well as
+   *         some default values.
+   */
+  private GridBagConstraints createSimpleGridBagConstraints(int columnIndex,
+          int rowIndex, int widthInColumns, int heightInRows) {
+    GridBagConstraints gridBagConstraints = new GridBagConstraints();
+    gridBagConstraints.gridx = columnIndex;
+    gridBagConstraints.gridy = rowIndex;
+    gridBagConstraints.gridwidth = widthInColumns;
+    gridBagConstraints.gridheight = heightInRows;
+    gridBagConstraints.fill = GridBagConstraints.BOTH;
+    gridBagConstraints.weightx = 1.0;
+    gridBagConstraints.weighty = 1.0;
 
-		return gridBagConstraints;
-	}
+    return gridBagConstraints;
+  }
 
-	/**
-	 * [CPPRCCC] Creates and shows the main (and only) window of the program.
-	 */
-	private MainWindow() {
-		// preconditions: none
-		super("EncyBuilder");
-		setLayout(new GridBagLayout());
+  /**
+   * Creates and shows the main (and only) window of the program.
+   */
+  private MainWindow() {
+    // preconditions: none
+    super("EncyBuilder");
+    setLayout(new GridBagLayout());
+    add(m_findEntryField, createSimpleGridBagConstraints(0, 0, 3, 1));
+    add(m_previousEntry, createSimpleGridBagConstraints(8, 1, 3, 1));
 
-		setSize(1000, 700);
-		setDefaultCloseOperation(EXIT_ON_CLOSE);
-		setVisible(true);
-		// postconditions: none (normal constructor)
-	}
+    setSize(1000, 700);
+    setDefaultCloseOperation(EXIT_ON_CLOSE);
+    setVisible(true);
+    // postconditions: none (normal constructor)
+  }
 
-	/**
-	 * [CPPRCCC] Starts the program by creating (and starting) the main window.
-	 * 
-	 * @param arguments
-	 *          the command-line arguments (not used at the moment)
-	 */
-	public static void main(String[] arguments) {
-		// preconditions: none
-		new MainWindow();
-		// postcondition: none
-	}
+  /**
+   * Starts the program by creating (and starting) the main window.
+   * 
+   * @param arguments the command-line arguments (not used at the moment)
+   */
+  public static void main(String[] arguments) {
+    // preconditions: none
+    new MainWindow();
+    // postcondition: none
+  }
 
 }
